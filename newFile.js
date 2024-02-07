e7f44d3875edf166d3e8e093b8c65bd15707080b
helcio <h.andre@giap.pl>
2024-02-07 16:06:56 +0100

    Complete phase-01, phase-02 and phase-04

--- newFile.js
+++ newFile.js
@@ -2,25 +2,19 @@
 Make fetch requests in the browser for each of the following phases.
 Paste your code for fetch requests here once you finish each phase.
 */
-
 /* ============================== Phase 1 ============================== */
-
-fetch("/products", {
-  method: "POST",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
-});
-
+// fetch("/products", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//   body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+// });
 /* ============================== Phase 2 ============================== */
-
-fetch("/products", {
-  method: "POST",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
-}).then(({ status, headers, url }) => console.log(status, headers, url));
-
+// fetch("/products", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//   body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+// }).then(({ status, headers, url }) => console.log(status, headers, url));
 /* ============================== Phase 3 ============================== */
-
 fetch("/products", {
   method: "POST",
   headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -31,3 +25,4 @@ fetch("/products", {
     categories: "Electronics",
   }),
 });
+console.log(searchParams, set);
